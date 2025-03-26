bc67a7737811a050551aab8f6f67d4e8d8108c65
sleepyghost-zzz <joshua.azro@gmail.com>
2025-03-25 21:25:41 -0400

    make it not blank/ trying to make the setip work

--- src/App.tsx
+++ src/App.tsx
@@ -5,85 +5,84 @@ import Box from "@mui/material/Box";
 import ScoringHeight from "./ScoringHeight";
 import { Stack } from "@mui/material";
 import GPToggle from "./GPToggle";
-import { NT4Provider, useEntry, useNt4 } from "@frc-web-components/react";
+import { useEntry, useNt4 } from "@frc-web-components/react";
 import Autonomy from "./Autonomy";
 import GPIndicator from "./GPIndicator";
 import ConnectionStatus from "./ConnectionStatus";
 import StationToggle from "./stationToggle";
-import Settings from "./Settings"; // Import Settings component
+import Settings from "./Settings";
 
-export default function App() {
+interface IPprops {
+  IP: string;
+  setIP: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function App({ IP, setIP }: IPprops) {
   const [intakeStation, setIntakeStation] = useEntry("/stationTarget", "left");
   const [coralHeight, setCoralHeight] = useEntry("/coralHeight", "level4");
-  const [algaeScoreHeight, setAlgaeScoreHeight] = useEntry(
-    "/algaeScoreHeight",
-    "level4"
-  );
-  const [algaeIntakeHeight, setAlgaeIntakeHeight] = useEntry(
-    "/algaeIntakeHeight",
-    "level3"
-  );
+  const [algaeScoreHeight, setAlgaeScoreHeight] = useEntry("/algaeScoreHeight", "level4");
+  const [algaeIntakeHeight, setAlgaeIntakeHeight] = useEntry("/algaeIntakeHeight", "level3");
   const [gamepiece, setGamepiece] = useEntry("/gpMode", "coral");
   const [autonomy, setAutonomy] = useEntry("/autonomyLevel", "smart");
   const [hasCoral, setHasCoral] = useEntry("/hasCoral", false);
   const [hasAlgae, setHasAlgae] = useEntry("/hasAlgae", false);
   const [isConnected, setConnected] = React.useState(false);
 
-  const [IP, setIP] = React.useState("10.4.1.2"); // IP state for your settings modal
+  const nt4 = useNt4();
 
-  useNt4().nt4Provider.addConnectionListener((conn: boolean) => {
-    setConnected(conn), true;
-  });
+  React.useEffect(() => {
+    nt4.nt4Provider.addConnectionListener((conn: boolean) => {
+      setConnected(conn);
+    }, true);
+  }, [nt4]);
 
   return (
-    <NT4Provider address={IP}> {/* Pass IP to NT4Provider */}
-      <Container sx={{ pl: 1 }} maxWidth={false} disableGutters>
-        <Box sx={{ m: 0, p: 0, zIndex: 1 }}>
-          <Typography variant="h3" component="h1" sx={{ m: 0, pb: 6 }}>
-            <Box sx={{ fontSize: 60, textAlign: "center", m: 0, pb: 0 }}>
-              SnakeScreen
-            </Box>
-          </Typography>
-          <Stack
-            direction={"row"}
-            spacing={20}
-            sx={{ my: 0, p: 0, justifyContent: "center" }}
-          >
-            <Stack direction={"column"} spacing={7} sx={{ px: 0, mx: 0 }}>
-              <GPToggle gamepiece={gamepiece} setGP={setGamepiece} />
-              <ScoringHeight
-                coralHeight={coralHeight}
-                setCoralHeight={setCoralHeight}
-                algaeScoreHeight={algaeScoreHeight}
-                setAlgaeScoreHeight={setAlgaeScoreHeight}
-                algaeIntakeHeight={algaeIntakeHeight}
-                setAlgaeIntakeHeight={setAlgaeIntakeHeight}
-              />
-            </Stack>
-            <Stack
-              direction={"column"}
-              spacing={20}
-              sx={{ px: 0, mx: 0, justifyContent: "space-around" }}
-            >
-              <StationToggle
-                station={intakeStation}
-                setStation={setIntakeStation}
-              />
-              <Autonomy autonomy={autonomy} setAutonomy={setAutonomy} />
-            </Stack>
-            <Stack
-              direction={"column"}
-              spacing={7}
-              sx={{ px: 0, mx: 0, justifyContent: "center" }}
-            >
-              <GPIndicator name=" Coral " value={hasCoral} />
-              <GPIndicator name="Algae" value={hasAlgae} />
-              <ConnectionStatus isConnected={isConnected} />
-            </Stack>
+    <Container sx={{ pl: 1 }} maxWidth={false} disableGutters>
+      <Box sx={{ m: 0, p: 0, zIndex: 1 }}>
+        <Typography variant="h3" component="h1" sx={{ m: 0, pb: 6 }}>
+          <Box sx={{ fontSize: 60, textAlign: "center", m: 0, pb: 0 }}>
+            SnakeScreen
+          </Box>
+        </Typography>
+        <Stack
+          direction={"row"}
+          spacing={20}
+          sx={{ my: 0, p: 0, justifyContent: "center" }}
+        >
+          <Stack direction={"column"} spacing={7} sx={{ px: 0, mx: 0 }}>
+            <GPToggle gamepiece={gamepiece} setGP={setGamepiece} />
+            <ScoringHeight
+              coralHeight={coralHeight}
+              setCoralHeight={setCoralHeight}
+              algaeScoreHeight={algaeScoreHeight}
+              setAlgaeScoreHeight={setAlgaeScoreHeight}
+              algaeIntakeHeight={algaeIntakeHeight}
+              setAlgaeIntakeHeight={setAlgaeIntakeHeight}
+            />
           </Stack>
-        </Box>
-      </Container>
-      <Settings IP={IP} setIP={setIP} /> {/* Settings is now part of App */}
-    </NT4Provider>
+          <Stack
+            direction={"column"}
+            spacing={20}
+            sx={{ px: 0, mx: 0, justifyContent: "space-around" }}
+          >
+            <StationToggle
+              station={intakeStation}
+              setStation={setIntakeStation}
+            />
+            <Autonomy autonomy={autonomy} setAutonomy={setAutonomy} />
+          </Stack>
+          <Stack
+            direction={"column"}
+            spacing={7}
+            sx={{ px: 0, mx: 0, justifyContent: "center" }}
+          >
+            <GPIndicator name="Coral" value={hasCoral} />
+            <GPIndicator name="Algae" value={hasAlgae} />
+            <ConnectionStatus isConnected={isConnected} />
+          </Stack>
+        </Stack>
+      </Box>
+      <Settings IP={IP} setIP={setIP} /> {}
+    </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,26 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
+import { NT4Provider } from "@frc-web-components/react";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement!);
 
 function Index() {
+  const [IP, setIP] = React.useState("localhost");
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App /> {}
-    </ThemeProvider>
+    <NT4Provider address={IP}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App IP={IP} setIP={setIP} />
+      </ThemeProvider>
+    </NT4Provider>
   );
 }
 
